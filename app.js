8d527ec02a8ac02108eba1b7130030aed892535e
juaniclausi <juaniclausi@gmail.com>
2020-03-12 15:43:09 -0300

    Usuario y Token listo

--- app.js
+++ app.js
@@ -1,9 +1,21 @@
 // Requires (importo librerias)
 var express = require('express');
 var mongoose = require('mongoose');
+var bodyParser = require('body-parser');
 
 // Inicializar Variables
 var app = express();
+
+// Body Parser (middleWares)
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }));
+// parse application/json
+app.use(bodyParser.json());
+
+// Importar rutas
+var usuarioRoutes = require('./routes/usuario');
+var loginRoutes = require('./routes/login');
+var appRoutes = require('./routes/app');
 
 // Conexión a la BBDD
 mongoose.connect('mongodb://localhost:27017/hospitalDB', {
@@ -19,15 +31,9 @@ mongoose.connect('mongodb://localhost:27017/hospitalDB', {
 });
 
 // Rutas
-app.get('/', (req, res, next) => {
-
-    // Realizo peticion y la formateo para que venga en JSON y mando un objeto
-    res.status(200).json({
-        ok: true,
-        mensaje: 'Petición realizada correctamente'
-    });
-
-});
+app.use('/usuario', usuarioRoutes);
+app.use('/login', loginRoutes);
+app.use('/', appRoutes);
 
 // Escuchar peticiones
 app.listen(3000, () => {
---
+++ middlewares/autenticacion.js
@@ -0,0 +1,37 @@
+var jwt = require('jsonwebtoken');
+
+var SEED = require('../config/config').SEED;
+
+// ===========================================
+// Verificar Token (middleWare)
+// ===========================================
+
+exports.verificaToken = function(req, res, next) {
+
+    // Recibo el token por el URL
+    var token = req.query.token;
+
+    // Verifico que el token sea valido
+    jwt.verify(token, SEED, (err, decoded) => {
+
+        if (err) {
+            return res.status(401).json({
+                ok: false,
+                mensaje: 'Token incorrecto',
+                errors: err
+            });
+        }
+
+        // Paso la información del usuario (que hace la peticion)
+        req.usuario = decoded.usuario;
+
+        // Continua con el servicio una vez que haya verificado el token
+        next();
+
+        // res.status(200).json({
+        //     ok: true,
+        //     decoded
+        // });
+
+    });
+}
